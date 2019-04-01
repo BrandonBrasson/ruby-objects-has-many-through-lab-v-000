2c8fb890a5835ca6c50653c22cd624b7d34693eb
Brandon Brasson <rocksong622@gmail.com>
2019-04-01 16:25:29 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,25 @@
+class Doctor
+  attr_accessor :name, :appointments
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def patients
+    self.appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
+end
